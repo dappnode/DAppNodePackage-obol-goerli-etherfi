7909a08096d182c43823cc024e89fd5ca07ecafc
Diego <dsimog01@estudiantes.unileon.es>
2023-06-28 17:34:16 +0200

    Remove comment

--- teku/entrypoint.sh
+++ teku/entrypoint.sh
@@ -15,5 +15,5 @@ exec /opt/teku/bin/teku --log-destination=CONSOLE \
   --validator-api-keystore-file=/cert/teku_client_keystore.p12 \
   --validator-api-keystore-password-file=/cert/teku_keystore_password.txt \
   --logging=ALL \
-  --validators-keystore-locking-enabled=false \ # To avoid teku restarting
+  --validators-keystore-locking-enabled=false \
   ${EXTRA_OPTS}
